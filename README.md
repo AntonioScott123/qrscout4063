0b29a61ac98765be56792807fdbf5a85b80d7fdf
AntonioSigma <antonio.ranger.scott@gmail.com>
2024-02-16 18:34:48 -0600

    SIGMASITE
    sigmas will rise

--- README.md
+++
@@ -1,8 +0,0 @@
-# NOTE
-We changed to github pages. Please use https://frc2713.github.io/QRScout/ until we redirect http://scout.redhawkrobotics.org
-
-# QRScout
-A QR Code Based Scouting System for FRC
-
-
-
--- src/main.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from 'preact'
-import { App } from './app.tsx'
-import './index.css'
-
-import { ThemeProvider } from "next-themes"
-
-render( <ThemeProvider attribute='class'>
-<App />
-</ThemeProvider>, document.getElementById('app')!)
