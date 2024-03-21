c1a413b586b58cf059b9ad159f395af5686b7533
AntonioScott <159655371+AntonioScott123@users.noreply.github.com>
2024-03-21 18:37:23 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,6 @@
 // Service Worker Logic
 
-// Define the cache name
 var cacheName = 'my-web-app-cache-v1';
-
-// List of files to cache
 var filesToCache = [
   '/',
   '/index.html',
@@ -22,6 +19,7 @@ var filesToCache = [
 
 // Install event
 self.addEventListener('install', function(event) {
+  // Precache the app shell
   event.waitUntil(
     caches.open(cacheName).then(function(cache) {
       return cache.addAll(filesToCache);
@@ -29,11 +27,41 @@ self.addEventListener('install', function(event) {
   );
 });
 
+// Activate event
+self.addEventListener('activate', function(event) {
+  // Clean up old caches if any
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(name) {
+          return name !== cacheName;
+        }).map(function(name) {
+          return caches.delete(name);
+        })
+      );
+    })
+  );
+});
+
 // Fetch event
 self.addEventListener('fetch', function(event) {
+  // Serve from cache if available, otherwise fetch from network
   event.respondWith(
     caches.match(event.request).then(function(response) {
       return response || fetch(event.request);
     })
   );
+});
+
+// Listen for beforeinstallprompt event
+self.addEventListener('beforeinstallprompt', function(event) {
+  // Cache the app when the user decides to install it
+  event.userChoice.then(function(choiceResult) {
+    if (choiceResult.outcome === 'accepted') {
+      console.log('User accepted the installation');
+      // You can cache files here if needed
+    } else {
+      console.log('User dismissed the installation');
+    }
+  });
 });
