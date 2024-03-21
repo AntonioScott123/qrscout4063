9c900eea674218720b8929fb84347439d0131c29
AntonioScott <159655371+AntonioScott123@users.noreply.github.com>
2024-03-21 18:38:19 -0500

    Update indexJavaScript.js

--- indexJavaScript.js
+++ indexJavaScript.js
@@ -30,12 +30,16 @@ const gameData = {
 		
 			
 			}
-
-			if ("serviceWorker" in navigator) {
-
-				navigator.serviceWorker.register("/service-worker.js");
-
-			}
+if ('serviceWorker' in navigator) {
+      window.addEventListener('load', function() {
+        navigator.serviceWorker.register('/service-worker.js')
+          .then(function(registration) {
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+          }, function(err) {
+            console.log('ServiceWorker registration failed: ', err);
+          });
+      });
+    }
 
 		function ClearAll() {
 			document.getElementById('prematch-scout-initials').value = '';
