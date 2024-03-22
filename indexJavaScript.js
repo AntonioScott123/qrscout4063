c1e7eab2aafa49ae245e7f3c38dc8d3ee3412ba9
AntonioScott <159655371+AntonioScott123@users.noreply.github.com>
2024-03-22 19:58:31 -0400

    trigonem

--- indexJavaScript.js
+++ indexJavaScript.js
@@ -30,16 +30,7 @@ const gameData = {
 		
 			
 			}
-if ('serviceWorker' in navigator) {
-      window.addEventListener('load', function() {
-        navigator.serviceWorker.register('/service-worker.js')
-          .then(function(registration) {
-            console.log('ServiceWorker registration successful with scope: ', registration.scope);
-          }, function(err) {
-            console.log('ServiceWorker registration failed: ', err);
-          });
-      });
-    }
+
 
 		function ClearAll() {
 			document.getElementById('prematch-scout-initials').value = '';
