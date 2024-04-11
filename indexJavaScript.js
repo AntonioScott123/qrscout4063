b8a053ed933a1ae5bad9bd39cd8dae2c7b087536
AntonioScott <159655371+AntonioScott123@users.noreply.github.com>
2024-04-11 16:44:15 -0500

    sigma fix?

--- indexJavaScript.js
+++ indexJavaScript.js
@@ -25,6 +25,11 @@ const gameData = {
 			
 			}
 
+
+var teamNumInput = document.getElementById('prematch-team-number');
+var matchNumInput = document.getElementById('prematch-match-number');
+var initialsInput = document.getElementById('initials');
+
 let smallify = {};
 
 smallify["Not_Observed"] = "NOB";
@@ -187,9 +192,6 @@ function ClearAll() {
 		}
 		// Update other fields similarly
 		
-		const initialsInput = document.getElementById('prematch-scout-initials');
-		const matchNumInput = document.getElementById('prematch-match-number');
-		const teamNumInput = document.getElementById('prematch-team-number');
 		
 		if (initialsInput.value === '') {
 			initialsInput.classList.add('error');
